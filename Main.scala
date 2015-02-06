cb24bfd67511bf7989848f64fb540524836f365c
adrien <adrien@pro-de-support.density.fr>
2015-02-06 12:51:04 +0100

    Second [part

--- Main.scala
+++ Main.scala
@@ -9,11 +9,17 @@ object Main extends App {
     val y = args(4).toInt
     val c = args(5)(0)
 
-    println(step(str, x, y, at(str, x, y), c).grouped(w).toList.map(_++"\n").flatten.mkString)
+    var visited: Set[(Int, Int)] = Set()
+
+    println(step(str, x, y, str(w*(y%h)+(x%w)), c).grouped(w).toList.map(_++"\n").flatten.mkString)
 
     def at(str: List[Char], x: Int, y: Int) = {
-      println(x, y)
-      if(x < 0 || x >= w || y < 0 || y >= h) ' ' else str(w*y+x)
+      val pos = (y%h, x%w)
+      if(visited.contains(pos)) ' '
+      else {
+        visited = visited + pos
+        str(w*(y%h)+(x%w))
+      }
     }
 
     def step(str: List[Char], x: Int, y: Int, c: Char, fill: Char) : List[Char] = {
@@ -22,7 +28,7 @@ object Main extends App {
           step(
             step(
               step(
-                str.patch(w*y+x, List(fill), 1), x+1, y, c, fill
+                str.patch(w*(y%h)+(x%w), List(fill), 1), x+1, y, c, fill
               ), x, y+1, c, fill
             ), x-1, y, c, fill
           ), x, y-1, c, fill
--- build.sbt
+++ build.sbt
@@ -1,3 +1,5 @@
 name := "Filler"
-scalaVersion := "2.11.4"
+
 version := "0.1"
+
+scalaVersion := "2.11.4"
